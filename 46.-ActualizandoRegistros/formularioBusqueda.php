8607f4d6c9181acb0f3df6fc48d0d32f52f86f3e
Armando Silverio Patricio <ing.armando.silverio@outlook.com>
2023-09-21 18:26:30 -0600

    Se agrego primera parte de ejemplo 'Buscar Registro' para actualizar registros

---
+++ 46.-ActualizandoRegistros/formularioBusqueda.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./style.css">
+    <title>Formulario de busqueda</title>
+</head>
+<body>
+
+    <form action="paginaBusqueda.php" method="get">
+        <label >Buscar:
+            <input type="text" name="buscarManual">
+            <input type="submit" value="Vamos" name="enviando">
+        </label>
+    </form>
+
+</body>
+</html>
